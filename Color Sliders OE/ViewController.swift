80bc4cc74d7fe697b673068d7bcbe1185ffc7a12
espinood <espinood@rose-hulman.edu>
2020-04-06 21:57:37 -0400

    Initial commit

--- Color Sliders OE/ViewController.swift
+++ Color Sliders OE/ViewController.swift
@@ -10,11 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var alphaSlider: UISlider!
+    
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    @IBOutlet weak var alphaLabel: UILabel!
+    
+    @IBOutlet weak var colorView: UIView!
+    
+    @IBAction func sliderChanged(_ sender: Any) {
+        updateView()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        redSlider.value = 0.71
+        greenSlider.value = 0.04
+        blueSlider.value = 0.22
+        alphaSlider.value = 1.00
+        updateView()
+        
     }
 
-
+    func updateView(){
+        print("Red: \(redSlider.value) Green: \(greenSlider.value) Blue: \(blueSlider.value) Alpha: \(alphaSlider.value)")
+        
+        //TODO: Update Labels
+        redLabel.text = String(format: "%.2f", redSlider.value)
+        greenLabel.text = String(format: "%.2f", greenSlider.value)
+        blueLabel.text = String(format: "%.2f", blueSlider.value)
+        alphaLabel.text = String(format: "%.2f", alphaSlider.value)
+        
+        //TODO: Update color view
+        colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: CGFloat(alphaSlider.value))
+    }
 }
 
